66bffd369bf1593870903009c259890981a73cac
Muhammad Asif <asif88383@gmail.com>
2023-10-24 16:52:02 +0500

    new updates

--- script.js
+++ script.js
@@ -26,6 +26,34 @@ Shery.imageEffect('#back', {
   gooey: true,
 })
 
+Shery.imageEffect('#imagediv img', {
+  style: 5,
+  config: {
+    a: { value: 1.98, range: [0, 30] },
+    b: { value: -0.69, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.6666666666666666 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.3, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+})
+
 var elems = document.querySelectorAll('.elem')
 elems.forEach(function (elem) {
   var h1s = elem.querySelectorAll('h1')
